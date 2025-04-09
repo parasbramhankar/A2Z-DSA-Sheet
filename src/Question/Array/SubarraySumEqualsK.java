44c42f0d4d9d834bc257f146230de4e6859c482b
parasbramhankar <parasbramhankar02@gmail.com>
2025-04-09 20:31:19 +0530

    SubArraySumEqualsK commit

---
+++ src/Question/Array/SubarraySumEqualsK.java
@@ -0,0 +1,39 @@
+package Question.Array;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SubarraySumEqualsK {
+
+    public static int subarraySum(int[] nums, int k) {
+        int sum = 0;
+        int ans = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                ans += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        int n=scanner.nextInt();
+
+        int[]arr=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]= scanner.nextInt();
+        }
+        System.out.println("Enter the sum: ");
+        int k= scanner.nextInt();
+
+        System.out.println("The total subArray: "+subarraySum(arr,k));
+
+    }
+}
